64cf3786a9a6b90b4d0d804e2434887a8bb2af0f
yuberee <59583743+yuberee@users.noreply.github.com>
2022-05-02 01:38:18 +0200

    Boss behavior WIP, Plants compile

--- code/entities/Boss.cs
+++ code/entities/Boss.cs
@@ -16,11 +16,20 @@ public enum BossState
 [Library( "xoxoxo_boss" )]
 [Model( Model = "models/citizen/citizen.vmdl" )]
 [Display( Name = "Boss", GroupName = "xoxoxo", Description = "The boss that walks around the office and looks for employees slacking off." )]
+[Hammer.Line("targetname", "pathtowardsexit")]
 public partial class Boss : AnimEntity
 {
 
 	public Clothing.Container Clothes = new();
 	public BossState CurrentState = BossState.Waiting;
+	[Net, Property, FGDType( "target_destination" )]
+	public string PathTowardsExit { get; set; } = "Exit_Path";
+	[Net, Property, FGDType( "target_destination" )]
+	public string PathTowardsStairs { get; set; } = "Stairs_Path";
+	[Net, Property, FGDType( "target_destination" )]
+	public string OfficeDoor { get; set; } = "Office_Door";
+	[Net, Property, FGDType( "target_destination" )]
+	public string ExitDoor { get; set; } = "Exit_Door";
 
 	public Boss()
 	{
@@ -47,7 +56,7 @@ public partial class Boss : AnimEntity
 	public Dictionary<BossState, float> StateSpeed => new Dictionary<BossState, float>()
 	{
 
-		[BossState.Waiting] = 110f,
+		[BossState.Waiting] = 80f,
 		[BossState.Walking] = 45f,
 		[BossState.Shouting] = 0f,
 		[BossState.Attacking] = 130f,
@@ -59,17 +68,119 @@ public partial class Boss : AnimEntity
 
 		SetModel( "models/citizen/citizen.vmdl" );
 		EnableDrawing = true;
+		Transmit = TransmitType.Always;
 		Clothes.DressEntity( this );
 
 		base.Spawn();
 
 	}
 
-	[Event.Tick]
+	float currentProgress = 0f;
+	bool backwards = false;
+	bool towardsStairs = false;
+
+	[Event.Tick.Server]
 	public void Animations()
 	{
 
 		SetAnimParameter( "move_x", StateSpeed[CurrentState] );
+
+		MovementPathEntity exitPath = FindByName( PathTowardsExit ) as MovementPathEntity;
+		MovementPathEntity stairsPath = FindByName( PathTowardsStairs ) as MovementPathEntity;
+
+		if ( exitPath.IsValid() && stairsPath.IsValid() )
+		{
+
+			MovementPathEntity currentPath = towardsStairs ? stairsPath : exitPath;
+			int currentNode = MathX.FloorToInt( currentProgress % currentPath.PathNodes.Count );
+			int nextNode = (currentNode + 1) % currentPath.PathNodes.Count;
+			float currentSpeed = 80f;
+			float currentDistance = currentPath.PathNodes[currentNode].Position.Distance( currentPath.PathNodes[nextNode].Position );
+
+			Vector3 newPosition = currentPath.GetPointBetweenNodes( currentPath.PathNodes[currentNode], currentPath.PathNodes[nextNode], currentProgress % 1 );
+
+			Rotation = Rotation.LookAt( newPosition.WithZ(0) - Position.WithZ(0), Vector3.Up );
+			Position = newPosition; 
+			
+			currentProgress += Time.Delta * (currentSpeed / currentDistance) * ( backwards ? -1 : 1 );
+
+			if ( nextNode == currentPath.PathNodes.Count - 1 && backwards == false && currentProgress % 1 >= 0.95f )
+			{
+
+				backwards = true;
+
+			}
+			else if ( currentNode == 0 && backwards == true && currentProgress % 1 <= 0.05f )
+			{
+
+				backwards = false;
+				towardsStairs = !towardsStairs;
+
+			}
+
+		}
+
+		DoorEntity officeDoor = FindByName( OfficeDoor ) as DoorEntity;
+
+		if ( officeDoor.IsValid() )
+		{
+
+			if ( this.Position.Distance( officeDoor.Position ) <= 100f )
+			{
+
+				if ( officeDoor.State == DoorEntity.DoorState.Closed )
+				{
+
+					officeDoor.Open();
+
+				}
+
+			}
+
+			if ( this.Position.Distance( officeDoor.Position ) >= 100f )
+			{
+
+				if ( officeDoor.State == DoorEntity.DoorState.Open )
+				{
+
+					officeDoor.Close();
+
+				}
+
+			}
+
+		}
+
+		DoorEntity exitDoor = FindByName( ExitDoor ) as DoorEntity;
+
+		if ( exitDoor.IsValid() )
+		{
+
+			if ( this.Position.Distance( exitDoor.Position ) <= 100f )
+			{
+
+				if ( exitDoor.State == DoorEntity.DoorState.Closed )
+				{
+
+					exitDoor.Open();
+
+				}
+
+			}
+
+			if ( this.Position.Distance( exitDoor.Position ) >= 90f )
+			{
+
+				if ( exitDoor.State == DoorEntity.DoorState.Open )
+				{
+
+					exitDoor.Close();
+
+				}
+
+			}
+
+		}
 
 	}
 
