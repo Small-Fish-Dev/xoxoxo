e5d0fbd0bcb239f1402b5122b908826e1f7f9cb6
yuberee <59583743+yuberee@users.noreply.github.com>
2023-03-04 22:51:03 +0100

    (Pain Month) Camera changes

---
+++ code/entities/Player.Camera.cs
@@ -0,0 +1,65 @@
+﻿using Sandbox;
+using System;
+
+public partial class Player
+{
+	[Net] public Vector3 TargetPosition { get; set; }
+	[Net] public Rotation TargetRotation { get; set; }
+	[Net] public bool IsDialogue { get; set; } = false;
+
+	public void ComputeCamera()
+	{
+
+		if ( !IsDialogue )
+		{
+
+			TargetPosition = xoxoxo.Instance.GameCamera.Position;
+			TargetRotation = xoxoxo.Instance.GameCamera.Rotation;
+
+		}
+
+		Camera.Position = Vector3.Lerp( Position, TargetPosition, Time.Delta * 5, true );
+		Camera.Rotation = Rotation.Lerp( Rotation, TargetRotation, Time.Delta * 5, true );
+
+	}
+
+	[Event( "StartDialogue" )]
+	public void StartDialogue( Dialogue dialogue )
+	{
+
+		IsDialogue = true;
+
+		Human speaker = dialogue.Speaker;
+		Vector3 lookAtPosition = speaker.Position + Vector3.Up * 64f;
+
+		TargetRotation = Rotation.LookAt( lookAtPosition - speaker.LookAtPosition, Vector3.Up );
+		TargetPosition = lookAtPosition + TargetRotation.Backward * 40f;
+
+		/*Log.Info( dialogue.Speaker );
+		Log.Info( dialogue.Text );
+		Log.Info( dialogue.Duration );
+		Log.Info( dialogue.Angry );
+		Log.Info( dialogue.TextSpeed );
+		Log.Info( TargetRotation );
+		Log.Info( TargetPosition );*/
+
+	}
+
+	[Event( "EndDialogue" )]
+	public void EndDialogue()
+	{
+
+		IsDialogue = false;
+
+	}
+	public void ComputePostProcess()
+	{
+
+		/*KissingPostProcess.DepthOfField.Enabled = true;
+		KissingPostProcess.DepthOfField.FocalLength = 340f - Math.Min( ( xoxoxo.Instance.Combo - 1f ) * 75f, 150f );
+		KissingPostProcess.DepthOfField.FocalPoint = 30000f;
+		KissingPostProcess.DepthOfField.Radius = MathF.Min( MathF.Pow( xoxoxo.Instance.Combo + 0.001f, 1.5f ), 100f );*/
+
+	}
+
+}
--- code/entities/Player.cs
+++ code/entities/Player.cs
@@ -1,12 +1,12 @@
 ﻿using Sandbox;
 using System;
 
-public partial class Player : Sandbox.Player
+public partial class Player : AnimatedEntity
 {
 
 	[Net] public Kisser Actor { get; set; }
 	public TimeSince LastKiss { get; private set; }
-	public StandardPostProcess KissingPostProcess { get; set; }
+	//public StandardPostProcess KissingPostProcess { get; set; }
 	public bool IsInCutscene { get; private set; } = false;
 
 	public override void Spawn()
@@ -14,17 +14,12 @@ public partial class Player : Sandbox.Player
 
 		base.Spawn();
 
-		CameraMode = new RoomCamera();
-
 	}
 
 	public override void ClientSpawn()
 	{
 
 		base.ClientSpawn();
-
-		KissingPostProcess = new StandardPostProcess();
-		PostProcess.Add( KissingPostProcess );
 
 	}
 
@@ -135,76 +130,11 @@ public partial class Player : Sandbox.Player
 
 	}
 
-	public void ComputePostProcess()
-	{
-
-		KissingPostProcess.DepthOfField.Enabled = true;
-		KissingPostProcess.DepthOfField.FocalLength = 340f - Math.Min( ( xoxoxo.Instance.Combo - 1f ) * 75f, 150f );
-		KissingPostProcess.DepthOfField.FocalPoint = 30000f;
-		KissingPostProcess.DepthOfField.Radius = MathF.Min( MathF.Pow( xoxoxo.Instance.Combo + 0.001f, 1.5f ), 100f );
-
-	}
-
 	[Event( "CharacterSelected" )]
 	public void CharacterSelected( string character )
 	{
 
 		Actor = xoxoxo.Instance.KisserLeft;
-
-	}
-
-}
-
-public partial class RoomCamera : CameraMode
-{
-
-	[Net, Predicted] public Vector3 TargetPosition { get; set; }
-	[Net, Predicted] public Rotation TargetRotation { get; set; }
-	[Net, Predicted] public bool IsDialogue { get; set; } = false;
-
-	public override void Update()
-	{
-
-		if ( !IsDialogue )
-		{
-
-			TargetPosition = xoxoxo.Instance.GameCamera.Position;
-			TargetRotation = xoxoxo.Instance.GameCamera.Rotation;
-
-		}
-
-		Position = Vector3.Lerp( Position, TargetPosition, Time.Delta * 5, true );
-		Rotation = Rotation.Lerp( Rotation, TargetRotation, Time.Delta * 5, true );
-
-	}
-
-	[Event("StartDialogue")]
-	public void StartDialogue( Dialogue dialogue )
-	{
-
-		IsDialogue = true;
-
-		Human speaker = dialogue.Speaker;
-		Vector3 lookAtPosition = speaker.Position + Vector3.Up * 64f;
-
-		TargetRotation = Rotation.LookAt( lookAtPosition - speaker.LookAtPosition, Vector3.Up );
-		TargetPosition = lookAtPosition + TargetRotation.Backward * 40f;
-
-		/*Log.Info( dialogue.Speaker );
-		Log.Info( dialogue.Text );
-		Log.Info( dialogue.Duration );
-		Log.Info( dialogue.Angry );
-		Log.Info( dialogue.TextSpeed );
-		Log.Info( TargetRotation );
-		Log.Info( TargetPosition );*/
-
-	}
-
-	[Event("EndDialogue")]
-	public void EndDialogue()
-	{
-
-		IsDialogue = false;
 
 	}
 
